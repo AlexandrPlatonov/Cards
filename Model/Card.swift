47cec626bff336102a2443fe0bf6be047f068eaf
Alexandr <alex280295@yandex.ru>
2022-05-12 17:35:36 +0700

    1

---
+++ Model/Card.swift
@@ -0,0 +1,24 @@
+
+
+import UIKit
+// типы фигуры карт
+enum CardType: CaseIterable {
+    case circle
+    case cross
+    case square
+    case fill
+}
+// цвета карт
+enum CardColor: CaseIterable {
+    case red
+    case green
+    case black
+    case gray
+    case brown
+    case yellow
+    case purple
+    case orange
+    case white
+}
+// игральная карточка
+typealias Card = (type: CardType, color: CardColor)
